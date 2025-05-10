da59cca804cfdc41c4725fb4634dffbb5cf90204
lvonw <91526397+lvonw@users.noreply.github.com>
2025-05-10 12:40:44 +0200

    Refined key calculation
    We now always have an integer amount of keys, percentual remainders are distributed evenly top down

--- Prize Pooler/prize-pooler.py
+++ Prize Pooler/prize-pooler.py
@@ -1,6 +1,7 @@
 import argparse
 import yaml
 import os
+import math
 
 PROJECT_PATH        = os.path.dirname(os.path.abspath(__file__))
 DISTRIBUTION_PATH   = os.path.join(PROJECT_PATH, "distributions.yml")
@@ -14,8 +15,7 @@ def prepare_arg_parser():
                         dest        = "pool",
                         required    = True, 
                         type        = int,
-                        help        = "Total amount of Keys")
-
+                        help        = "Total amount of Keys.")
     parser.add_argument("-m",
                         "--mode",
                         dest        = "mode",
@@ -23,7 +23,7 @@ def prepare_arg_parser():
                         nargs       = 1,
                         type        = str,
                         choices     = ["s", "d"],
-                        help        = "What are we calculating for?")
+                        help        = "What distribution are we calculating for?")
     
     return parser
 
@@ -61,20 +61,72 @@ def validate_distribution(distribution):
 
     return valid
 
-def format_prize_pool (pool, distribution):
-    print (f"Total Pool: {pool}")
+def calculate_prizes (pool, distribution):
+    prize_pool = {}
+    
+    # Distribute keys over all divs
+    key_total = 0
     for div, shares in distribution.items():
-        div_pool = pool * (shares["share"] / 100.0)
-        print (f"{div}: Total {div_pool:.2f} Keys")
+        div_pool = {}
+        
+        div_pool["p"]       = shares["share"]
+        keys                = math.floor(pool * (shares["share"] / 100))
+        div_pool["keys"]    = keys
+        key_total           += keys
+
+        rank_distribution = []
+        for share in shares["distribution"]:
+            rank_share = {}
+            rank_share["p"] = share
+            rank_distribution.append(rank_share)
+        div_pool["distribution"] = rank_distribution
+
+        prize_pool[div] = div_pool
+
+    key_remainder   = pool - key_total
+    divs            = list(distribution.keys())
+    i               = 0
+    while key_remainder > 0:
+        prize_pool[divs[i]]["keys"] += 1
+        i = (i + 1) % len(divs)
+        key_remainder -= 1
+
+    # Distribute keys within each div
+    for div, shares in prize_pool.items():
+        div_keys = shares["keys"]
+
+        
+        key_total = 0
+        for rank_share in shares["distribution"]:
+            rank_keys = math.floor(div_keys * (rank_share["p"] / 100))
+            rank_share["keys"] = rank_keys
+            key_total += rank_keys
+
+        key_remainder   = div_keys - key_total
+        i               = 0
+        while key_remainder > 0:
+            shares["distribution"][i]["keys"] += 1
+            i = (i + 1) % len(shares["distribution"])
+            key_remainder -= 1
+
+    return prize_pool
+
+
+def format_prize_pool (pool, prizes):
+    formatted = ""
+    formatted += f"Total Pool: {pool} Keys \n\n"
+
+    for div, shares in prizes.items():
+        formatted += f"{div}: {shares['p']:5.2f} % | Total {shares['keys']:3.0f} Keys\n"
 
         i = 1
         for share in shares["distribution"]:
-            rank_share = div_pool * (share / 100.0)
-            print (f"\t{i}: {rank_share:.2f} Keys")
+            formatted += f"   {i}: {share['p']:5.2f} % | Total {share['keys']:3.0f} Keys\n"
             i +=1
 
+        formatted += "\n"
 
-
+    return formatted
 
 def main():
     parser  = prepare_arg_parser()
@@ -89,9 +141,10 @@ def main():
     if not validate_distribution(distribution):
         return -1
     
-    print (args.pool)
-    format_prize_pool(args.pool, distribution)
+    prizes = calculate_prizes(args.pool, distribution)
+    formatted = format_prize_pool(args.pool, prizes)
 
+    print (formatted)
 
 
 if __name__ == "__main__":
